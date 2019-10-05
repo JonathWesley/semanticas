5985ce3195fb34b5505a97306283021b8f028c41
Jonath Wesley Herdt <jonathherdt@gmail.com>
2019-10-05 19:30:37 -0300

    first commit

---
+++ TrabM2/src/trabm2/Semantico.java
@@ -0,0 +1,48 @@
+package trabm2;
+
+import java.util.Stack;
+
+public class Semantico implements Constants
+{
+  Stack stack = new Stack();
+
+  public int getResult()
+  {
+    return ((Integer)stack.peek()).intValue();
+  }
+
+  public void executeAction(int action, Token token) throws SemanticError
+  {
+    Integer a, b;
+
+    switch (action)
+    {
+      case 1:
+        String tmp = currentToken.getLexeme();
+        if (tmp.charAt(0) == '0')
+          throw new SemanticError("Números começados por 0 não são permitidos", token.getPosition());
+        stack.push(Integer.valueOf(tmp));
+        break;
+      case 2:
+        b = (Integer) stack.pop();
+        a = (Integer) stack.pop();
+        stack.push(new Integer(a.intValue() + b.intValue()));
+        break;
+      case 3:
+        b = (Integer) stack.pop();
+        a = (Integer) stack.pop();
+        stack.push(new Integer(a.intValue() - b.intValue()));
+        break;
+      case 4:
+        b = (Integer) stack.pop();
+        a = (Integer) stack.pop();
+        stack.push(new Integer(a.intValue() * b.intValue()));
+        break;
+      case 5:
+        b = (Integer) stack.pop();
+        a = (Integer) stack.pop();
+        stack.push(new Integer(a.intValue() / b.intValue()));
+        break;
+    }
+  }
+}
---
+++ TrabM2/src/trabm2/TrabM2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabm2;
+
+/**
+ *
+ * @author jonat
+ */
+public class TrabM2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
